e21e1f567c0b26ec60b56d52b80620e7971798b0
Wenzhuo Zhao <zslyvain@gmail.com>
2020-10-23 18:14:05 +0200

    Supprimer des lignes de code inutilies de TME1; mettre à jour le .gitigonre

--- Java/src/com/paracamplus/ilp1/ilp1tme1/test/MyCompilerTest.java
+++ Java/src/com/paracamplus/ilp1/ilp1tme1/test/MyCompilerTest.java
@@ -1,25 +1,16 @@
 package com.paracamplus.ilp1.ilp1tme1.test;
 
-import com.paracamplus.ilp1.compiler.CompilationException;
 import com.paracamplus.ilp1.compiler.test.CompilerRunner;
 import com.paracamplus.ilp1.compiler.test.CompilerTest;
-import com.paracamplus.ilp1.parser.ParseException;
-import org.junit.Test;
 import org.junit.runners.Parameterized;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Collection;
 
 public class MyCompilerTest extends CompilerTest {
     protected static String[] samplesDirName = { "SamplesTME1" };
     public MyCompilerTest(File file) {
         super(file);
-    }
-
-    @Test
-    public void test() throws CompilationException, ParseException, IOException {
-        processFile();
     }
 
     @Parameterized.Parameters(name = "{0}")
--- Java/src/com/paracamplus/ilp1/ilp1tme1/test/MyInterpreterTest.java
+++ Java/src/com/paracamplus/ilp1/ilp1tme1/test/MyInterpreterTest.java
@@ -1,25 +1,16 @@
 package com.paracamplus.ilp1.ilp1tme1.test;
 
-import com.paracamplus.ilp1.interpreter.interfaces.EvaluationException;
 import com.paracamplus.ilp1.interpreter.test.InterpreterRunner;
 import com.paracamplus.ilp1.interpreter.test.InterpreterTest;
-import com.paracamplus.ilp1.parser.ParseException;
-import org.junit.Test;
 import org.junit.runners.Parameterized;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Collection;
 
 public class MyInterpreterTest extends InterpreterTest {
     protected static String[] samplesDirName = { "SamplesTME1" };
     public MyInterpreterTest(File file) {
         super(file);
-    }
-
-    @Test
-    public void test() throws ParseException, IOException, EvaluationException {
-        processFile();
     }
 
     @Parameterized.Parameters(name = "{0}")
