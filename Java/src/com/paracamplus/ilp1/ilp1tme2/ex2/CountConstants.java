f7ea1001f0d1e08fc937d18e49dce97eceae2267
Wenzhuo Zhao <zslyvain@gmail.com>
2020-10-26 00:32:29 +0100

    Supprimer des affichages inutiles

--- Java/src/com/paracamplus/ilp1/ilp1tme2/ex2/CountConstants.java
+++ Java/src/com/paracamplus/ilp1/ilp1tme2/ex2/CountConstants.java
@@ -21,37 +21,31 @@ public class CountConstants implements IASTvisitor<Integer, Integer, Exception> 
     public Integer visit(IASTbinaryOperation iast, Integer data) throws Exception {
         data += iast.getLeftOperand().accept(this,data);
         data += iast.getRightOperand().accept(this,data);
-        System.out.println("a binary operation data: " + data);
         return data;
     }
 
     @Override
     public Integer visit(IASTblock iast, Integer data) throws Exception {
-        System.out.println("before block data: "+data);
         IASTblock.IASTbinding[] bindings = iast.getBindings();
         for(IASTblock.IASTbinding binding : bindings){
             data += binding.getInitialisation().accept(this,data);
         }
         //data += iast.getBody().accept(this,data);
-        System.out.println("after block data: "+data);
         return data;
     }
 
     @Override
     public Integer visit(IASTboolean iast, Integer data) throws Exception {
-        System.out.println("a boolean");
         return 1;
     }
 
     @Override
     public Integer visit(IASTfloat iast, Integer data) throws Exception {
-        System.out.println("a float");
         return 1;
     }
 
     @Override
     public Integer visit(IASTinteger iast, Integer data) throws Exception {
-        System.out.println("a integer");
         return 1;
     }
 
@@ -61,7 +55,6 @@ public class CountConstants implements IASTvisitor<Integer, Integer, Exception> 
         for(IASTexpression argument : arguments){
             argument.accept(this,data);
         }
-        System.out.println("invocation data: "+data);
         return data;
     }
 
@@ -76,20 +69,17 @@ public class CountConstants implements IASTvisitor<Integer, Integer, Exception> 
 
     @Override
     public Integer visit(IASTstring iast, Integer data) throws Exception {
-        System.out.println("a string data: " + data);
         return 1;
     }
 
     @Override
     public Integer visit(IASTunaryOperation iast, Integer data) throws Exception {
         data += iast.getOperand().accept(this,data);
-        System.out.println("an unary operation data: " + data);
         return data;
     }
 
     @Override
     public Integer visit(IASTvariable iast, Integer data) throws Exception {
-        System.out.println("a variable data:" + data);
         return 0;
     }
 }
